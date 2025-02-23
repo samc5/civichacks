f11d164fc357ed75d0c7bbc6f29e027d043bd8a5
GregoryFan <gfan1316@bu.edu>
2025-02-22 23:32:36 -0500

    Mongo Debug

--- src/app/api/submit/route.js
+++ src/app/api/submit/route.js
@@ -5,13 +5,14 @@ export async function POST(req) {
   try {
     const { MONGODB_URI } = process.env;
     const client = new MongoClient(MONGODB_URI);
+    console.log("MONGODB_URI:", process.env.MONGODB_URI ? "Loaded" : "Missing");
     await client.connect();
     const db = client.db("ICE");
     const collection = db.collection("Events");
 
     const formData = await req.json();
     const result = await collection.insertOne(formData);
-
+    
     client.close();
     return NextResponse.json({ message: "Form submitted successfully", result }, { status: 201 });
   } catch (error) {
